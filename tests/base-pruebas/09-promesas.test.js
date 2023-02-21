deb0ce125799539abc5349f16b5b112e91efd496
Carlota <carlota.gallart@gmail.com>
2023-02-21 11:39:46 +0100

    test con tareas asincronas

--- tests/base-pruebas/09-promesas.test.js
+++ tests/base-pruebas/09-promesas.test.js
@@ -1,10 +1,31 @@
 import { getHeroeByIdAsync } from "../../src/base-pruebas/09-promesas";
 describe('Prueba 09-promesas', () => { 
-    test('Debe de retornar un heroe', () => { 
+    test('Debe de retornar un heroe', (done) => { 
         const id = 1
         getHeroeByIdAsync(id)
         .then(hero => {
-            // expect(true).toBe(false)
+            expect(hero).toEqual({
+                id: 1,
+                name: 'Batman',
+                owner: 'DC'
+            })
+            done();
+        })
+
+     })
+     test('Debe de obtener un error si el heroe no existe', (done) => { 
+        const id = 100;
+        getHeroeByIdAsync(id)
+        .then(hero =>{
+            expect(hero).toBeFalsy();
+            done();
+        })
+        .catch(error => {
+            // console.log(error)
+            expect(error).toBe( `No se pudo encontrar el héroe ` + id);
+
+            // expect(error).toBe( `No se pudo encontrar el héroe ${id}`);
+            done();
         })
 
      })
