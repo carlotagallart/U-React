e1a368ba6c8f95e04258ff0f34eb6f419cbc55e9
Carlota <carlota.gallart@gmail.com>
2023-02-21 17:30:00 +0100

    test por props

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -4,7 +4,7 @@ export const FirstApp = ({title, subtitle, name }) => {
 
     return(
         <>
-        <h1>{title}</h1>
+        <h1 data-testid="test-title">{ title }</h1>
         {/* <code>{JSON.stringify(newMessage)}</code> */}
         <p>{subtitle}</p>
         <p>{name}</p>
--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -2,16 +2,24 @@ import { render } from '@testing-library/react';
 import { FirstApp } from '../src/FirstApp';
 
 describe('Pruebas en <FirstApp />', () => { 
-    test('debe de hacer match con el snapshot', () => { 
-        const title = 'Hola, soy Victoria'
-        const { container } = render( <FirstApp title={title}/>);
-        expect( container ).toMatchSnapshot();
-     });
+    // test('debe de hacer match con el snapshot', () => { 
+    //     const title = 'Hola, soy Victoria'
+    //     const { container } = render( <FirstApp title={title}/>);
+    //     expect( container ).toMatchSnapshot();
+    //  });
      test('debe de mostrar el titulo en un h1', () => { 
         const title = 'Hola, soy Victoria'
-        const { container, getByText } = render( <FirstApp title={title}/>);
+        const { container, getByText, getByTestId } = render( <FirstApp title={title}/>);
         expect( getByText(title) ).toBeTruthy();
-        const h1 = container.querySelector('h1');
-        expect(h1.innerHTML).toContain(title);
+        // const h1 = container.querySelector('h1');
+        // expect(h1.innerHTML).toContain(title);
+        expect(getByTestId('test-title').innerHTML).toContain(title)
+
       })
+      test('debe de mostrar el subtitulo enviado por props', () => { 
+        const title = 'Hola, soy Victoria'
+        const subTitle = 'Soy el subtitulo'
+        const { getAllByText } = render( <FirstApp title={title} subtitle={subTitle}/>);
+        expect( getAllByText(subTitle).length ).toBe(1)
+     })
  })
